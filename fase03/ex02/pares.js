3ac9f0c0a102a537feda2a3fc7cc0ffcdf74fd8e
carolinemnascimento <59872911+carolinemnascimento@users.noreply.github.com>
2020-02-04 10:05:04 -0300

    k3

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -2,8 +2,8 @@ function pares(numero) {
     var numero = numero
     var total = numero % 2;
     if (total == 0){
-        return ("Os números sào pares")
+        return ("Os numeros sÃo pares")
     }else{
-        return ("Os números nào sào pares")
+        return ("Os numeros nÃo sÃo pares")
     }
 }
