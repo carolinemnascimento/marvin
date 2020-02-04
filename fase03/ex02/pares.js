9e1bfe5917a217c521ad419198b7e945c7231e23
carolinemnascimento <59872911+carolinemnascimento@users.noreply.github.com>
2020-02-04 11:17:42 -0300

    certo

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,9 +1,8 @@
 function pares(num1, num2){
-    var numero = (num1, num2)
-    var total = numero % 2;
-    if (total === 0){
+   
+    if (num1 % 2 === 0 && num2 % 2 === 0){
         return ("Os números são pares")
-    }else{
+    } else{
         return ("Os números não são pares")
     }
 }
