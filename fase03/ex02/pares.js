4f5fd158e1f1c882118c604b0f6313da393aad39
carolinemnascimento <59872911+carolinemnascimento@users.noreply.github.com>
2020-02-04 10:25:31 -0300

    kk3

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,9 +1,9 @@
-function pares(numero) {
-    var numero = numero
+function pares(num1, num2) {
+    var numero = (num1, num2)
     var total = numero % 2;
     if (total == 0){
-        return ("Os numeros s√o pares")
+        return ("Os numeros s„o pares")
     }else{
-        return ("Os numeros n√o s√o pares")
+        return ("Os numeros n„o s„o pares")
     }
 }
