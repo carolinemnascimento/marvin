4c63506c0804ce77d60abd88c372be12b3399074
carolinemnascimento <59872911+carolinemnascimento@users.noreply.github.com>
2020-02-20 11:19:57 -0300

    6.3

---
+++ fase06/ex03/precoMedio.js
@@ -0,0 +1,18 @@
+function precoMedio (arroz){
+    let med=0;
+    let max= -1;
+    let resp= "";
+    let i = 0;
+    
+    whille (i<arguments.length){
+        med += arguments[i].preco;
+        if( arguments[i].preco>max){
+            max=arguments[i].preco;
+            resp=arguments[i].nome;
+        }
+        i++;
+    }
+    
+    console.log("O produto mais caro se chama \"" + resp + "\"")
+    return med/arguments.length;
+}
